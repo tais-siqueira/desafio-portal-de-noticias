5eec2a84f1c575ccdf434698e42de7e071d4d129
tais-siqueira <tais.siqueira15@hotmail.com>
2023-12-11 00:35:15 -0300

    atualizando 'admin'

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from base.models import  Categorias, Noticia, Cadastro
 
-from base.models import  Categorias, Noticia, Comentario, Cadastro
 # Register your models here.
 @admin.register(Cadastro)
 class CadastroAdmin(admin.ModelAdmin):
@@ -9,9 +9,9 @@ class CadastroAdmin(admin.ModelAdmin):
 
 @admin.register(Categorias)
 class CategoriasAdmin(admin.ModelAdmin):
-    list_display = ['categorias']
-    search_fields = ['categorias']
-    list_filter = ['categorias']
+    list_display = ['categoria']
+    search_fields = ['categoria']
+    list_filter = ['categoria']
     
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
@@ -19,10 +19,6 @@ class NoticiaAdmin(admin.ModelAdmin):
     search_fields = ['titulo', 'user']
     list_filter = ['data']
     
-@admin.register(Comentario)
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ['texto', 'data']
-    search_fields = ['texto', 'data']
     
     
     
